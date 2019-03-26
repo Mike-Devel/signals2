f89236ced6b0f877b2fc3ba2afbbfd2cfd1f1488
Mike Dev <mike.dev@gmx.de>
2019-03-26 20:46:38 +0100

    [Standalone] Merge Signal/Slot templates into main header files

--- include/boost/signals2/slot.hpp
+++ include/boost/signals2/slot.hpp
@@ -12,6 +12,7 @@
 #ifndef BOOST_SIGNALS2_SLOT_HPP
 #define BOOST_SIGNALS2_SLOT_HPP
 
+#include <boost/signals2/detail/variadic_arg_type.hpp>
 #include <boost/bind.hpp>
 #include <boost/function.hpp>
 #include <boost/mpl/identity.hpp>
@@ -21,6 +22,116 @@
 #include <boost/signals2/slot_base.hpp>
 #include <boost/visit_each.hpp>
 
-#include <boost/signals2/variadic_slot.hpp>
+namespace boost {
+namespace signals2 {
+template<typename Signature, typename SlotFunction = boost::function<Signature> >
+class slot;
+
+template< typename SlotFunction,
+	typename R,
+	typename ... Args>
+	class slot  <R(Args...), SlotFunction>
+	: public slot_base, public detail::std_functional_base<Args...>
+
+{
+public:
+	template<typename prefixSignature, typename OtherSlotFunction>
+	friend class slot;
+
+	typedef SlotFunction slot_function_type;
+	typedef R result_type;
+	typedef typename mpl::identity<R(Args...)>::type signature_type;
+
+	template<unsigned n> class arg {
+	public:
+		typedef typename detail::variadic_arg_type<n, Args...>::type type;
+	};
+	static constexpr int arity = sizeof...(Args);
+
+	template<typename F>
+	slot(const F& f)
+	{
+		init_slot_function(f);
+	}
+	// copy constructors
+
+	template<typename Signature, typename OtherSlotFunction>
+	slot(const slot<Signature, OtherSlotFunction>& other_slot) :
+		slot_base(other_slot), _slot_function(other_slot._slot_function)
+	{
+	}
+	// bind syntactic sugar
+	template<typename A1, typename A2, typename ... BindArgs>
+	slot(const A1& arg1, const A2& arg2, const BindArgs& ... args)
+	{
+		init_slot_function(boost::bind(arg1, arg2, args...));
+	}
+	// invocation
+	R operator()(Args ... args)
+	{
+		locked_container_type locked_objects = lock();
+		return _slot_function(args...);
+	}
+	R operator()(Args ... args) const
+	{
+		locked_container_type locked_objects = lock();
+		return _slot_function(args...);
+	}
+	// tracking
+	slot& track(const std::weak_ptr<void>& tracked) {
+		_tracked_objects.push_back(tracked);
+		return *this;
+	}
+	slot& track(const signal_base& signal)
+	{
+		track_signal(signal);
+		return *this;
+	}
+	slot& track(const slot_base& slot)
+	{
+		tracked_container_type::const_iterator it;
+		for (it = slot.tracked_objects().begin(); it != slot.tracked_objects().end(); ++it) {
+			_tracked_objects.push_back(*it);
+		}
+		return *this;
+	}
+	template<typename ForeignWeakPtr>
+	slot& track_foreign(const ForeignWeakPtr& tracked,
+						typename weak_ptr_traits<ForeignWeakPtr>::shared_type* /*SFINAE*/ = 0)
+	{
+		_tracked_objects.push_back(detail::foreign_void_weak_ptr(tracked));
+		return *this;
+	}
+	template<typename ForeignSharedPtr>
+	slot& track_foreign(const ForeignSharedPtr& tracked,
+						typename shared_ptr_traits<ForeignSharedPtr>::weak_type* /*SFINAE*/ = 0)
+	{
+		_tracked_objects.push_back
+		(
+			detail::foreign_void_weak_ptr
+			(
+				typename shared_ptr_traits<ForeignSharedPtr>::weak_type(tracked)
+			)
+		);
+		return *this;
+	}
+
+	const slot_function_type& slot_function() const { return _slot_function; }
+	slot_function_type& slot_function() { return _slot_function; }
+private:
+	template<typename F>
+	void init_slot_function(const F& f)
+	{
+		_slot_function = detail::get_invocable_slot(f, detail::tag_type(f));
+		signals2::detail::tracked_objects_visitor visitor(this);
+		boost::visit_each(visitor, f);
+	}
+
+	SlotFunction _slot_function;
+};
+
+} // end namespace signals2
+} // end namespace boost
+
 
 #endif // BOOST_SIGNALS2_SLOT_HPP
